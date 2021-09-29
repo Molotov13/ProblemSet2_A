2e5fbc63cd9f17194c44849e9fbd0c8a7e48b0c0
Molotov <844632982@qq.com>
2021-09-29 09:35:18 -0700

    Update ProblemSet2_A.java

--- src/ProblemSet2_A.java
+++ src/ProblemSet2_A.java
@@ -4,8 +4,8 @@
  * ProblemSet 2-A
  */
 public class ProblemSet2_A {
-    public static void main(String[] args) {
-        Calculator item = new Calculator();
-        item.display();
+    public static void main(String[] args) { // Create a driver class
+        Calculator item = new Calculator(); // Create a Calculator object 
+        item.display(); // Display all formula
     }
 }
